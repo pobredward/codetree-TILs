3cb7aa15a0aecd38890a0d2ded26d2fcedae08da
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-05 18:31:29 +0900

    Title: 따옴표 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240505/따옴표 출력/print-quote.cpp
+++ 240505/따옴표 출력/print-quote.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cout << "He says \"It's a really simple sentence\"."
+    cout << "He says \"It's a really simple sentence\".";
     return 0;
 }
