f465ca970e1612d7544fb74a83eb72964a5fa778
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-06 16:53:39 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << "9.2ft = " << 9.2*30.48 << "cm\n";
+    cout << "1.3mi = " << 1.3*160934 << "cm";
+    return 0;
+}
