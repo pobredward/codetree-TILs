f7e6dfd69b194146aba1719f86a05d760fa7f0af
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-06 16:48:59 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240506/소수점 반올림하기/rounding-decimal-points.cpp
+++ 240506/소수점 반올림하기/rounding-decimal-points.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+
 int main() {
     // 여기에 코드를 작성해주세요.
     double a = 25.352;
 
-    std::cout << fixed;
+    std::cout << std::fixed;
     std::cout.precision(1);
 
     std::cout << a;
