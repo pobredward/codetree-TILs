fa817283fe681fad9ad1e3e5384ef357a0297baf
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-12 14:41:12 +0900

    Title: 입력받은 값 교체하기, Time: 311ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240512/입력받은 값 교체하기/changing-inputs.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(a, b);
