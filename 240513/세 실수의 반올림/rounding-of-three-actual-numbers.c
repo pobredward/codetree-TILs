be7d71731aa410050ee073489dc36a3678e1c43f
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-13 12:47:54 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b, c;
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+
+    printf("%.3f\n", a);
+    printf("%.3f\n", b);
+    printf("%.3f\n", c);
+
+    return 0;
+}
