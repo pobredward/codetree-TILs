b1d00272d89273aefc95edac46900c7ec46667fb
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-14 19:30:35 +0900

    Title: 날짜 변경하여 출력 2, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240514/날짜 변경하여 출력 2/print-date-with-different-format-2.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+let input = fs.readFileSync(0).toString().trim().split("-");
+
+let m = input[0];
+let d = input[1];
+let y = input[2];
+
+console.log("%d.%d.%d", y, m, d);
