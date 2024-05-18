7108c5b937d1e18404d5ea9e9b6e3dcdb9d7985e
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-18 15:41:53 +0900

    Title: 출석 부르기, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240518/출석 부르기/calling-attendance.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+let input = fs.readFileSync(0).toString()
+
+let num = Number(input);
+
+if (num==1) {
+    console.log("John");
+} else if (num==2) {
+    console.log("Tom");
+} else if (num==3) {
+    console.log("Paul");
+} else {
+    console.log("Vacancy");
+}
