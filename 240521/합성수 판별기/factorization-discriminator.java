01357672d645dc099d4de9233595d34485331d57
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-21 23:56:18 +0900

    Title: 합성수 판별기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/합성수 판별기/factorization-discriminator.java
+++ 240521/합성수 판별기/factorization-discriminator.java
@@ -9,7 +9,7 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 System.out.println("C");
                 cnt++;
