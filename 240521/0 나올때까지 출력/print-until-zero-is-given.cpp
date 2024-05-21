62bfa35c8dd7650a375637360584cde3207a644b
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-21 23:50:46 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240521/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while (1) {
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        cout << n << "\n"; 
+    }
+    return 0;
+}
