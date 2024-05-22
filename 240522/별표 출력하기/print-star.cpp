12210685cf3415ace7d559c1676ef9c326ed9943
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-22 12:37:56 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240522/별표 출력하기/print-star.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
