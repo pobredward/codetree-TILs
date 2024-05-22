53a0e972014c8a44180c877211cd6031c6db6e3d
Edward Shin <100953765+pobredward@users.noreply.github.com>
2024-05-22 10:46:59 +0900

    Title: 정사각형 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240522/정사각형 별표 출력하기/print-stars-in-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
